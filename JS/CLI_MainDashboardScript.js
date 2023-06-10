7c625901d405a3018f8119375d361ffa5c15082a
Samytheturtle <samy.srzcln@gmail.com>
2023-06-10 15:27:06 -0600

    MAIN DASHBOARD 100% falta funcionalidad con la API

--- JS/CLI_MainDashboardScript.js
+++ JS/CLI_MainDashboardScript.js
@@ -1,10 +1,28 @@
 window.onload = function() {
     div_resultados = document.getElementById("div_resultados");
     div_resultados.style.display = "none";
-
+    changerCategory();
+    changerProduct();
+    showUserType();
+    countProductCar();
 }
 
-//document.getElementById("carrito").style.display = "block";
+function showUserType(){
+    var userLabel = document.getElementById("user-label");
+    userLabel.textContent = "Nombre de usuario - Tipo de usuario";
+}
+
+
+function buttonFuncionalityRegisteraddress() {
+    alert("Funcionalidad en desarrollo: Registrar dirección de entrega");
+    return;
+}
+
+function buttonFuncionalityRegisterPaymentMethod() {
+    alert("Funcionalidad en desarrollo: Registrar método de pago");
+    return;
+}
+
 
 function openTab(event, tabName) {
     var tabContent = document.getElementsByClassName("tab-content");
@@ -25,8 +43,21 @@ function openTab(event, tabName) {
     }else if(tabName==="carrito"){
         changerProductCar();
         console.log('CACA');
+        updateCarrinfo();
     }else if(tabName=="visualizar"){
-        changerProductHisroty();
+        changerProductHistory();
+    }
+}
+
+function changerProductHistory() {
+    div_resultados.style.display = "flex";
+    var tarjetaContenedor = document.querySelector(".tarjeta-contenedorProductosHistorial");
+    tarjetaContenedor.innerHTML = "";
+
+    for (var i = 0; i < 50; i++) {
+        var n = i;
+        let tarjeta = generateCardProductHistory(n);
+        tarjetaContenedor.insertAdjacentHTML("beforeend", tarjeta);
     }
 }
 function changerProductCar() {
@@ -34,18 +65,24 @@ function changerProductCar() {
     var tarjetaContenedor = document.querySelector(".tarjeta-contenedorProductosCarrito");
     tarjetaContenedor.innerHTML = "";
 
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 50; i++) {
         var n = i;
-        let tarjeta = generateCardProduct(n);
+        let tarjeta = generateCardProductCar(n);
         tarjetaContenedor.insertAdjacentHTML("beforeend", tarjeta);
     }
 }
+
+function countProductCar(){
+    var cartCountLabel = document.getElementById("cantidad_carrito");
+    cartCountLabel.textContent = "100";
+}
+
 function changerProduct() {
     div_resultados.style.display = "flex";
     var tarjetaContenedor = document.querySelector(".tarjeta-contenedorProductos");
     tarjetaContenedor.innerHTML = "";
 
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 50; i++) {
         var n = i;
         let tarjeta = generateCardProduct(n);
         tarjetaContenedor.insertAdjacentHTML("beforeend", tarjeta);
@@ -68,7 +105,52 @@ function generateCardCategory(n) {
         '</div>';
 }
 function generateCardProduct(n) {
+    var nombreProducto = "Producto " + n; 
     return '<div class="tarjetaProduct">' +
-        '<h3>' + n + ', ' + "Prueba" + '</h3>' +
+        '<h3>' + nombreProducto + '</h3>' +
+        '<button onclick="addToCar(\'' + nombreProducto + '\')">Agregar al carrito</button>' +
         '</div>';
+}
+function generateCardProductCar(n) {
+    var nombreProducto = "Producto " + n; 
+    return '<div class="tarjetaProduct">' +
+        '<h3>' + nombreProducto + '</h3>' +
+        '</div>';
+}
+function generateCardProductHistory(n) {
+    var nombreProducto = "Producto " + n; 
+    return '<div class="tarjetaProduct">' +
+        '<h3>' + nombreProducto + '</h3>' +
+        '</div>';
+}
+function addToCar(nombreProducto) {
+    // Lógica para agregar el producto al carrito
+    alert("Agregado al carrito: " + nombreProducto);
+}
+function updateCarrinfo() {
+    var carritoCantidadLabel = document.getElementById("carrito-cantidad-label");
+    carritoCantidadLabel.textContent = "Cantidad: " + cantidadCarrito;
+
+    var carritoTotalLabel = document.getElementById("carrito-total-label");
+    carritoTotalLabel.textContent = "Total: $" + totalCarrito.toFixed(2);
+}
+function deleteAllcar() {
+    // Lógica para borrar el contenido del carrito
+    alert("Carrito borrado correctamente");
+
+    // Reiniciar el carrito
+    cantidadCarrito = 0;
+    totalCarrito = 0.00;
+
+    updateCarrinfo();
+}
+function payCar() {
+    // Lógica para procesar el pago del carrito
+    alert("Carrito pagado correctamente");
+
+    // Reiniciar el carrito
+    cantidadCarrito = 0;
+    totalCarrito = 0.00;
+
+    updateCarrinfo();
 }
